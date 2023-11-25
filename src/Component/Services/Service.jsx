999fa1dd03dead83a2901c2ff7b8185fb38abafe
Jay yadav <yadavjay365@gmail.com>
2023-11-25 19:27:20 +0530

    first commit

---
+++ src/Component/Services/Service.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+ function Service(){
+    return(
+        <>
+        <div className="">
+           <img src="https://png.pngtree.com/background/20211217/original/pngtree-abstract-bg-picture-image_1596862.jpgcd " alt="" />
+            <h1 className=" text-3xl text-black ">OUR OFFRINGS</h1>
+            <div className=" flex  bg-[#006A4E] mt-8 border-none p-4">
+                <div className=""><span text-lg>COMPLIANCE CHECKING</span> 
+                <p>Detailed compliance checking of various applicable Acts </p><p> and laws such as Companies Act, Income tax Act, FEMA, PMLA, PF Act.</p>
+                </div>
+
+                <div className=""><span className="text-lg">FINANCIAL PERFORMANCE MONITORING</span> 
+                <p>Preparation of financial statement.</p>
+                <p>Review of historical financial performance based of Financial statements.</p>
+                </div>
+
+                <div className=""><span className=" text-lg ">BUDGET FORMATION & MONITORING</span> 
+                <p>Formation of Budget.</p>
+                <p>Regular monitoring and comparison of Actual Vs Budgeted performance.</p>
+                </div>
+            </div>
+            <div className="flex bg-white">
+                <div className=" m-8">
+                    <span className="text-lg">MANAGEMENT INFORMATION SYSTEM</span>
+                    <p>Preparation and presentation of management Information System report which is essential for management, stakeholders.and investor to understand performance efficiency of business</p>
+                </div>
+                <div className=" m-8">
+                    <span className="text-lg" >SCRUTINY OF FINANCIAL STATEMENT</span>
+                    <p>Detailed scrutiny and thorough checking of financial statement from forensic point of view.Comprehensive checking of audit report issued by authorities such a Chartered Accountants, Other Statutory auditors..</p>
+                    
+                </div>
+                <div className=" m-8">
+                    <span className="text-lg">PERIODICAL MEETINGS</span>
+                    <p>Periodical meetings, frequency based on requirement of client, But we are just as accessible as one of the team members via Phone call, Video call and Email. </p>
+                    
+                    
+                </div>
+
+
+            </div>
+            <div className=" flex m-8 bg-[#006A4E] p-4">
+                <div className=""><span text-lg>COMPLIANCE CHECKING</span> 
+                <p>Detailed compliance checking of various applicable Acts </p><p> and laws such as Companies Act, Income tax Act, FEMA, PMLA, PF Act.</p>
+                </div>
+
+                <div className=""><span className="text-lg">FINANCIAL PERFORMANCE MONITORING</span> 
+                <p>Preparation of financial statement.</p>
+                <p>Review of historical financial performance based of Financial statements.</p>
+                </div>
+
+                <div className=""><span className=" text-lg ">BUDGET FORMATION & MONITORING</span> 
+                <p>Formation of Budget.</p>
+                <p>Regular monitoring and comparison of Actual Vs Budgeted performance.</p>
+                </div>
+            </div>
+            
+
+
+
+        </div>
+        </>
+    )
+ }
+ export default Service
---
+++ src/main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Route } from 'react-router-dom'
+import './index.css'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './Component/About/About.jsx'
+import Home from './Component/Home/Home.jsx'
+import Contact from './Component/Contact/Contact.jsx'
+import Service from './Component/Services/Service.jsx'
+
+// const router=createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children:[
+//       {
+//         path:'/',
+//         element: <Home/>
+//       },
+//       {
+//         path:'about',
+//         element:<About/>
+
+//       },
+//       {
+//         path:'contact',
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+
+// ])
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+   <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='contact' element={<Contact/>}/>
+    <Route path='service' element={<Service/>}/>
+    
+   </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
